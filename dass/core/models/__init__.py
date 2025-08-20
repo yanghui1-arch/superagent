0f8a132cd85275e99156087e651686ae8d4d087f
dass90 <3053034939@qq.com>
2025-08-20 21:41:39 +0800

    feat: add todo list models

--- dass/core/models/__init__.py
+++ dass/core/models/__init__.py
@@ -1,8 +1,89 @@
 from uuid import UUID
 from uuid import uuid4
-from typing import Optional
+from typing import Optional, Literal, ClassVar
 from pydantic import BaseModel
 from ...kits.tool import Tool, ToolResult, ResultFlag
+from ..prompts import NO_COMPLETED_TAG, COMPLETED_TAG
+
+class TODOItem(BaseModel):
+    """ todo item 
+    
+    Args:
+        order(int): item order. Higher -> Later, Lower -> Earlier. Same -> Meanwhile executing.
+        content(str): item concrete content.
+        status(Literal["completed", "no-completed"]): item current status. Default to `no-completed`
+    """
+    
+    no_completed_tag: ClassVar[str] = NO_COMPLETED_TAG
+    completed_tag: ClassVar[str] = COMPLETED_TAG
+
+    order: int
+    content: str
+    status: Literal["completed", "no-completed"] = "no-completed"
+
+    def complete(self) -> bool:
+        """ to sign the item completed
+        Replace `- []` to `- [x]`
+
+        Returns:
+            bool: whether complete successfully.
+        """
+
+        if self.status == "completed":
+            print(f"[WARNING]: todo item `{self.content}` has been completed before.")
+        self.status = "completed"
+        self.content = self.content.replace(TODOItem.no_completed_tag, TODOItem.completed_tag, 1)
+        return True
+
+
+class TODOList(BaseModel):
+    """ todo list
+    Agent create a todo list and complete every item step by step. Some items can be executed at the same time if TODOItem orders are the same.
+
+    Args:
+        plan_list(list[TODOItem]): a todo list
+        completed_cnt(int): TODOItem counts that have been completed. Default to 0.
+        no_completed_cnt(int): TODOItem counts that have not completed. Default to 0.
+    """
+    plan_list: list[TODOItem]
+    completed_cnt: int = 0
+    no_completed_cnt: int = 0
+
+    def model_post_init(self, context):
+        """ initialize counts based on todo list """
+        self.no_completed_cnt = sum(1 for item in self.plan_list if item.status == "no-completed")
+        self.completed_cnt = len(self.plan_list) - self.no_completed_cnt
+    
+    def complete_plan(self, todo_item_idx:int) -> bool:
+        """ update todo item status and completed, no-completed counts
+
+        Args:
+            todo_item_idx(int): index of todo item. NOT order!! MUST be idx!!
+
+        Returns:
+            bool: whether complete succussfully
+        """
+
+        if todo_item_idx >= len(self.plan_list) or todo_item_idx < 0:
+            raise ValueError(f"todo_item_idx cannot be {todo_item_idx}. Please ensure its range is in [0, {len(self.plan_list)})")
+        
+        todo_item = self.plan_list[todo_item_idx]
+        completed = todo_item.complete()
+        if completed:    
+            self.completed_cnt += 1
+            self.no_completed_cnt -= 1
+
+        if self.no_completed_cnt < 0:
+            print(f"[WARNING]: no-completed count is below 0 when complete plan.")
+        return completed
+
+    @property
+    def completed_cnt(self):
+        return self.completed_cnt
+
+    @property
+    def no_completed_cnt(self):
+        return self.no_completed_cnt
 
 class Action(BaseModel):
     """ Action by agent
